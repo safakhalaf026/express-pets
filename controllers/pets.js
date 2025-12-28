7afc575031ed2512c81f54e778ca54ea1396a31a
safakhalaf026 <safakhalaf026@gmail.com>
2025-12-28 15:55:21 +0300

    added create pet route

--- controllers/pets.js
+++ controllers/pets.js
@@ -4,6 +4,13 @@ const app = express()
 const router = express.Router()
 
 router.post('/', async (req,res) => {
+    try {
+        const newPet = await Pet.create(req.body)
+        res.status(201).json({newPet})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error:'failed to create pet'})
+    }
     res.json(req.body)
 })
 
