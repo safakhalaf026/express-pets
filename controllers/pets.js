a50ae4fc7ae8bb99bc67c736e87b35407b59977a
safakhalaf026 <safakhalaf026@gmail.com>
2025-12-29 09:20:01 +0300

    added all pet routes

--- controllers/pets.js
+++ controllers/pets.js
@@ -11,7 +11,60 @@ router.post('/', async (req,res) => {
         console.log(error)
         res.status(500).json({error:'failed to create pet'})
     }
-    res.json(req.body)
 })
 
+router.get('/', async (req,res)=>{
+    try {
+        const allPets = await Pet.find()
+        res.status(200).json({allPets})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error:'failed to get pet data'})
+    }
+})
+
+router.get('/:petId', async (req,res)=>{
+    try {
+        const {petId} = req.params // destructure id from req.params
+        const onePet = await Pet.findById(petId)
+        if(!onePet){
+            res.status(404).json({error:'Record Not Found'})
+        }else{
+            res.status(200).json({onePet})
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error:'Internal Server Error'})
+    }
+})
+
+router.delete('/:petId', async (req,res)=>{
+    try {
+        const {petId} = req.params // destructure id from req.params
+        const onePet = await Pet.findByIdAndDelete(petId)
+        if(!onePet){
+            res.status(404).json({error:'Record Not Found'})
+        }else{
+            res.status(200).json({onePet})
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error:'Internal Server Error'})
+    }
+})
+
+router.put('/:petId', async (req,res)=>{
+    try {
+        const {petId} = req.params // destructure id from req.params
+        const onePet = await Pet.findByIdAndUpdate(petId, req.body)
+        if(!onePet){
+            res.status(404).json({error:'Record Not Found'})
+        }else{
+            res.status(200).json({onePet})
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error:'Internal Server Error'})
+    }
+})
 module.exports = router
